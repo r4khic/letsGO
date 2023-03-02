1909f7df999ce766d962eb7a57f491df352fd30e
r4khic <r4khic@gmail.com>
2023-03-02 21:24:14 +0600

    Finished chapter 4 Isolated the application routes

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -17,22 +17,14 @@ func main() {
 	flag.Parse()
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
-
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 	infoLog.Printf("Starting server on %s", *addr)
 	err := srv.ListenAndServe()
